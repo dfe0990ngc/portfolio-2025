17fa7279a6fab380e22c498936642b00747b76d6
dfe0990ngc <dfe0990ngc@gmail.com>
2025-07-23 15:18:16 +0800

    - Added necessary images for project illustrations

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+      <p>Nelson Cañete's Portfolio</p>
     </div>
   );
 }
